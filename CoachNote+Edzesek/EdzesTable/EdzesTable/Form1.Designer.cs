8a9fff65398371fbfd6742932fd6c31b66b5c796
kircsa08 <kircsa08@gmail.com>
2018-12-20 14:20:38 +0100

    megvan minden button

--- CoachNote+Edzesek/EdzesTable/EdzesTable/Form1.Designer.cs
+++ CoachNote+Edzesek/EdzesTable/EdzesTable/Form1.Designer.cs
@@ -30,6 +30,12 @@
         {
             this.dataGridViewEdzesek = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.buttonModosit = new System.Windows.Forms.Button();
+            this.buttonMegse = new System.Windows.Forms.Button();
+            this.buttonTorol = new System.Windows.Forms.Button();
+            this.buttonUj = new System.Windows.Forms.Button();
+            this.buttonMent = new System.Windows.Forms.Button();
+            this.buttonKilep = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEdzesek)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,28 +44,96 @@
             this.dataGridViewEdzesek.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewEdzesek.Location = new System.Drawing.Point(12, 12);
             this.dataGridViewEdzesek.Name = "dataGridViewEdzesek";
-            this.dataGridViewEdzesek.Size = new System.Drawing.Size(577, 425);
+            this.dataGridViewEdzesek.Size = new System.Drawing.Size(662, 425);
             this.dataGridViewEdzesek.TabIndex = 0;
+            this.dataGridViewEdzesek.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewEdzesek_CellValueChanged);
+            this.dataGridViewEdzesek.DefaultValuesNeeded += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridViewEdzesek_DefaultValuesNeeded);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(666, 37);
+            this.button1.Location = new System.Drawing.Point(694, 38);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Betöltés";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonModosit
+            // 
+            this.buttonModosit.Location = new System.Drawing.Point(694, 90);
+            this.buttonModosit.Name = "buttonModosit";
+            this.buttonModosit.Size = new System.Drawing.Size(75, 23);
+            this.buttonModosit.TabIndex = 2;
+            this.buttonModosit.Text = "Módosít";
+            this.buttonModosit.UseVisualStyleBackColor = true;
+            // 
+            // buttonMegse
+            // 
+            this.buttonMegse.Location = new System.Drawing.Point(694, 143);
+            this.buttonMegse.Name = "buttonMegse";
+            this.buttonMegse.Size = new System.Drawing.Size(75, 23);
+            this.buttonMegse.TabIndex = 3;
+            this.buttonMegse.Text = "Mégse";
+            this.buttonMegse.UseVisualStyleBackColor = true;
+            this.buttonMegse.Click += new System.EventHandler(this.buttonMegse_Click);
+            // 
+            // buttonTorol
+            // 
+            this.buttonTorol.Location = new System.Drawing.Point(694, 198);
+            this.buttonTorol.Name = "buttonTorol";
+            this.buttonTorol.Size = new System.Drawing.Size(75, 23);
+            this.buttonTorol.TabIndex = 4;
+            this.buttonTorol.Text = "Töröl";
+            this.buttonTorol.UseVisualStyleBackColor = true;
+            this.buttonTorol.Click += new System.EventHandler(this.buttonTorol_Click);
+            // 
+            // buttonUj
+            // 
+            this.buttonUj.Location = new System.Drawing.Point(694, 258);
+            this.buttonUj.Name = "buttonUj";
+            this.buttonUj.Size = new System.Drawing.Size(75, 23);
+            this.buttonUj.TabIndex = 5;
+            this.buttonUj.Text = "Új Edzés";
+            this.buttonUj.UseVisualStyleBackColor = true;
+            this.buttonUj.Click += new System.EventHandler(this.buttonUj_Click);
+            // 
+            // buttonMent
+            // 
+            this.buttonMent.Location = new System.Drawing.Point(694, 319);
+            this.buttonMent.Name = "buttonMent";
+            this.buttonMent.Size = new System.Drawing.Size(75, 23);
+            this.buttonMent.TabIndex = 6;
+            this.buttonMent.Text = "Mentés";
+            this.buttonMent.UseVisualStyleBackColor = true;
+            this.buttonMent.Click += new System.EventHandler(this.buttonMent_Click);
+            // 
+            // buttonKilep
+            // 
+            this.buttonKilep.Location = new System.Drawing.Point(694, 379);
+            this.buttonKilep.Name = "buttonKilep";
+            this.buttonKilep.Size = new System.Drawing.Size(75, 23);
+            this.buttonKilep.TabIndex = 7;
+            this.buttonKilep.Text = "Kilépés";
+            this.buttonKilep.UseVisualStyleBackColor = true;
+            this.buttonKilep.Click += new System.EventHandler(this.buttonKilep_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonKilep);
+            this.Controls.Add(this.buttonMent);
+            this.Controls.Add(this.buttonUj);
+            this.Controls.Add(this.buttonTorol);
+            this.Controls.Add(this.buttonMegse);
+            this.Controls.Add(this.buttonModosit);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridViewEdzesek);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEdzesek)).EndInit();
             this.ResumeLayout(false);
 
@@ -69,6 +143,12 @@
 
         private System.Windows.Forms.DataGridView dataGridViewEdzesek;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonModosit;
+        private System.Windows.Forms.Button buttonMegse;
+        private System.Windows.Forms.Button buttonTorol;
+        private System.Windows.Forms.Button buttonUj;
+        private System.Windows.Forms.Button buttonMent;
+        private System.Windows.Forms.Button buttonKilep;
     }
 }
 
--- CoachNote+Edzesek/EdzesTable/EdzesTable/Form1.cs
+++ CoachNote+Edzesek/EdzesTable/EdzesTable/Form1.cs
@@ -16,6 +16,7 @@ namespace EdzesTable
     {
         private MySQLDatabaseInterface mdi;
         private DataTable edzesekdt;
+        bool lettValtozas = false;
         public Form1()
         {
             InitializeComponent();
@@ -23,6 +24,7 @@ namespace EdzesTable
 
         private void button1_Click(object sender, EventArgs e)
         {
+            betoltesutaniAllapot();
             dataGridViewEdzesek.ReadOnly = true;
             dataGridViewEdzesek.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dataGridViewEdzesek.AllowUserToDeleteRows = false;
@@ -30,6 +32,151 @@ namespace EdzesTable
             mdi.open();
             edzesekdt = mdi.getToDataTable("SELECT * FROM edzes");
             dataGridViewEdzesek.DataSource = edzesekdt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BelepeskoriAllapot();
+        }
+        public void betoltesutaniAllapot()
+        {
+            buttonMegse.Visible = true;
+            buttonMent.Visible = true;
+            buttonModosit.Visible = true;
+            buttonTorol.Visible = true;
+            buttonUj.Visible = true;
+            buttonMegse.Enabled = false;
+            buttonMent.Enabled = false;
+            buttonModosit.Enabled = true;
+            buttonTorol.Enabled = true;
+            buttonUj.Enabled = true;
+        }
+        public void modositoAllapot()
+        {
+            buttonMegse.Enabled = true;
+            buttonMent.Enabled = true;
+            buttonTorol.Enabled = false;
+            buttonUj.Enabled = false;
+        }
+        public void BelepeskoriAllapot()
+        {
+            buttonMegse.Visible = false;
+            buttonMent.Visible = false;
+            buttonModosit.Visible = false;
+            buttonTorol.Visible = false;
+            buttonUj.Visible = false;
+        }
+
+        private void buttonMent_Click(object sender, EventArgs e)
+        {
+            if (lettValtozas)
+            {
+                mdi.updateChangesInTable(edzesekdt);
+                buttonMent.Enabled = false;
+                buttonModosit.Enabled = true;
+                buttonUj.Enabled = true;
+                buttonTorol.Enabled = true;
+                lettValtozas = false;
+            }
+            else
+            {
+                MessageBox.Show("Nem lett módosítva egy adat sem.", "Módosítás", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                buttonMent.Enabled = false;
+            }
+        }
+
+        private void dataGridViewEdzesek_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            lettValtozas = true;
+        }
+
+        private void buttonUj_Click(object sender, EventArgs e)
+        {
+            modositoAllapot();
+            dataGridViewEdzesek.AllowUserToAddRows = true;
+            dataGridViewEdzesek.SelectionMode = DataGridViewSelectionMode.CellSelect;
+
+            int sor = dataGridViewEdzesek.Rows.Count - 1;
+            dataGridViewEdzesek.Rows[sor].Cells[1].Selected = true;
+            dataGridViewEdzesek.Rows[sor].Cells[1].Value = "Írja ide az új adatot";
+
+            dataGridViewEdzesek.ReadOnly = false;
+            for (int i = 0; i < sor; i = i + 1)
+            {
+                dataGridViewEdzesek.Rows[i].ReadOnly = true;
+            }
+            dataGridViewEdzesek.FirstDisplayedScrollingRowIndex = sor;
+            dataGridViewEdzesek.Columns[0].ReadOnly = true;
+            lettValtozas = true;
+        }
+
+        private void buttonKilep_Click(object sender, EventArgs e)
+        {
+            if (lettValtozas)
+            {
+                if (MessageBox.Show("Nem mentett adatok vannak! Valóban ki akar lépni?", "Kilépés", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    mdi.close();
+                    this.Close();
+                }
+            }
+            else
+            {
+                mdi.close();
+                this.Close();
+            }
+        }
+
+        private void buttonTorol_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Valóban törölni akarja a sort?", "Törlés", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+            {
+                try
+                {
+                    int sor = dataGridViewEdzesek.SelectedRows[0].Index;
+                    dataGridViewEdzesek.Rows.RemoveAt(sor);
+                    buttonMent.Enabled = true;
+                    lettValtozas = true;
+                }
+                catch (Exception ex)
+                {
+                    return;
+                }
+            }
+        }
+
+        private void dataGridViewEdzesek_DefaultValuesNeeded(object sender, DataGridViewRowEventArgs e)
+        {
+            Adatbazis ujida = new Adatbazis();
+            MySQLDatabaseInterface mdiujid = ujida.kapcsolodas();
+            mdiujid.open();
+
+            int max;
+            bool siker = int.TryParse(mdiujid.executeScalarQuery("SELECT MAX(edzesid) FROM edzes"), out max);
+            if (!siker)
+            {
+                MessageBox.Show("Nem lehet megállapítani a következő rekord kulcsát. Adatbázis lekérdezési hiba. Új adat felvitele nem lehetséges", "Hiba...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            mdiujid.close();
+
+            e.Row.Cells[0].Value = max + 1;
+        }
+
+        private void buttonMegse_Click(object sender, EventArgs e)
+        {
+            buttonUj.Enabled = true;
+            buttonModosit.Enabled = true;
+            buttonTorol.Enabled = true;
+
+            dataGridViewEdzesek.ReadOnly = true;
+            dataGridViewEdzesek.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            buttonMegse.Visible = false;
+
+            edzesekdt = mdi.getToDataTable("SELECT * FROM vtermekek"); dataGridViewEdzesek.DataSource = edzesekdt;
+
+            lettValtozas = false;
         }
     }
 }
