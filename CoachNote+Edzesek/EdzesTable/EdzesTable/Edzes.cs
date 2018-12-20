919fce487f1b23dc671776f9e780c575a6517267
kircsa08 <35925724+kircsa08@users.noreply.github.com>
2018-12-20 13:44:39 +0100

    Add files via upload

---
+++ CoachNote+Edzesek/EdzesTable/EdzesTable/Edzes.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdzesekGridView
+{
+    class Edzes
+    {
+        private int edzesid;
+        private DateTime edzesidopont;
+        private string nap;
+        private string tipus;
+        private string helyszin;
+        private bool megtartva;
+        public Edzes(int edzesid, DateTime edzesidopont, string nap, string tipus, string helyszin, bool megtartva)
+        {
+            this.edzesid = edzesid;
+            this.edzesidopont = edzesidopont;
+            this.nap = nap;
+            this.tipus = tipus;
+            this.helyszin = helyszin;
+            this.megtartva = megtartva;
+        }
+        public int getEdzesId()
+        {
+            return edzesid;
+        }
+        public DateTime getEdzesIdopont()
+        {
+            return edzesidopont;
+        }
+        public void setMeccsIdopont(DateTime edzesidopont)
+        {
+            this.edzesidopont = edzesidopont;
+        }
+        public string getNap()
+        {
+            return nap;
+        }
+        public void setNap(string nap)
+        {
+            this.nap = nap;
+        }
+        private string getTipus()
+        {
+            return tipus;
+        }
+        private void setTipus(string tipus)
+        {
+            this.tipus = tipus;
+        }
+        private string getHelyszin()
+        {
+            return helyszin;
+        }
+        private void setHelyszin(string helyszin)
+        {
+            this.helyszin = helyszin;
+        }
+        private bool getMegtartva()
+        {
+            return megtartva;
+        }
+        private void setMegtartva(bool megtartva)
+        {
+            this.megtartva = megtartva;
+        }
+    }
+}
---
+++ CoachNote+Edzesek/EdzesTable/EdzesTable/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using ConnectToMysqlDatabase;
+
+namespace EdzesTable
+{
+    public partial class Form1 : Form
+    {
+        private MySQLDatabaseInterface mdi;
+        private DataTable edzesekdt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridViewEdzesek.ReadOnly = true;
+            dataGridViewEdzesek.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewEdzesek.AllowUserToDeleteRows = false;
+            Adatbazis a = new Adatbazis(); mdi = a.kapcsolodas();
+            mdi.open();
+            edzesekdt = mdi.getToDataTable("SELECT * FROM edzes");
+            dataGridViewEdzesek.DataSource = edzesekdt;
+        }
+    }
+}
